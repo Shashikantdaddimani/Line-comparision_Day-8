d84f1066617cc1e433233c9ea5e144e61433633a
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-03 06:46:21 +0530

    Added a display message program

---
+++ src/com/line/comparison/LineComparison.java
@@ -0,0 +1,10 @@
+package com.line.comparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the line comparison computation program");
+	}
+
+}
