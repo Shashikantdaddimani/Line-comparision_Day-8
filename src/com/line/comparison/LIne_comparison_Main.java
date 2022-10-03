a969d432bc443e62b5c0652b23dc2be3506fab2b
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-03 07:23:08 +0530

    Added Oops concepts Line and points as well as Equals and compare method

---
+++ src/com/line/comparison/LIne_comparison_Main.java
@@ -0,0 +1,12 @@
+package com.line.comparison;
+
+public class LIne_comparison_Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comparison computation program Oops concept");
+		LineComparison lc = new LineComparison();
+		lc.getLength();
+		lc.compareLines();
+
+	}
+
+}
